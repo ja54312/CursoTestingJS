659a6b47b3a738024b5fa852aa4cfb91a58d7763
ja54312 <ija54312@gmail.com>
2022-10-28 11:10:56 -0500

    eslint

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,25 +1,25 @@
-const {sum,multiply,divide} = require('./02-math')
+const { sum, multiply, divide } = require('./02-math');
 
-test("adds 1 + 3  should be 4",()=>{
-  const rta = sum(1,3)
-  expect(rta).toBe(4)
-})
+test('adds 1 + 3  should be 4', () => {
+  const rta = sum(1, 3);
+  expect(rta).toBe(4);
+});
 
-test("should be 4",()=>{
-  const rta = multiply(1,4)
-  expect(rta).toBe(4)
-})
+test('should be 4', () => {
+  const rta = multiply(1, 4);
+  expect(rta).toBe(4);
+});
 
-test("should divide",()=>{
-  const rta = divide(6,3)
-  expect(rta).toBe(2)
-  const rta2 = divide(5,2)
-  expect(rta2).toBe(2.5)
-})
+test('should divide', () => {
+  const rta = divide(6, 3);
+  expect(rta).toBe(2);
+  const rta2 = divide(5, 2);
+  expect(rta2).toBe(2.5);
+});
 
-test("should divide for zero",()=>{
-  const rta = divide(6,0)
-  expect(rta).toBe(null)
-  const rta2 = divide(5,0)
-  expect(rta2).toBe(null)
-})
+test('should divide for zero', () => {
+  const rta = divide(6, 0);
+  expect(rta).toBe(null);
+  const rta2 = divide(5, 0);
+  expect(rta2).toBe(null);
+});
