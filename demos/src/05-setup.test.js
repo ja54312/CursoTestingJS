583a6464f74377132ddc3e2f172fd2d74126e782
ja54312 <ija54312@gmail.com>
2022-11-01 21:19:32 -0600

    Setup and Teardown

---
+++ demos/src/05-setup.test.js
@@ -0,0 +1,31 @@
+describe('group 1', () => {
+  beforeAll(() => {
+    console.log('beforeAll');
+    // up db
+  });
+  afterAll(() => {
+    console.log('afterAll');
+    // down  db
+  });
+  beforeEach(() => {
+    console.log('each');
+  });
+  afterEach(() => {
+    console.log('after each');
+  });
+  test('case 1', () => {
+    expect(1 + 1).toBe(2);
+  });
+  test('case 2', () => {
+    expect(1 + 3).toBe(4);
+  });
+});
+
+describe('group 2', () => {
+  test('case 3', () => {
+    expect(1 + 1).toBe(2);
+  });
+  test('case 4', () => {
+    expect(1 + 3).toBe(4);
+  });
+});
